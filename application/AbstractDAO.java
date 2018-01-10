4d000a2021502143c73870c6a92498a4bc792bad
jarqprog <jarqprog@gmail.com>
2018-01-10 21:14:09 +0100

    add fill AbstractDAO class with empty methods

--- application/AbstractDAO.java
+++ application/AbstractDAO.java
@@ -1,10 +1,50 @@
 package application;
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+
 public abstract class AbstractDAO
 {
 
     protected String defaultFileName;
     protected String defaultFilePath;
+    protected List<String> loadedData;
 
+    public String getDefaultFileName(){
+        return defaultFileName;
+    }
+
+    public String getDefaultFilePath(){
+        return defaultFilePath;
+    }
+
+    public void setDefaultFileName(String name){
+        defaultFileName = name;
+    }
+
+    public void setDefaultFilePath(String path){
+        defaultFilePath = path;
+    }
+
+    public List<String> getDataFromFile(){
+        List<String> outputData = new ArrayList<String>(); // tmp
+        return outputData;
+    }
+
+    public Boolean checkIfFileExist(){
+
+        return true;
+    }
+
+    public void saveData(List<String>){
+
+
+    }
+    public void createDefaultFile(){
+
+
+    }
 
 }
