5f86285c2edaa10375e24808518aab3582385f9a
jarqprog <jarqprog@gmail.com>
2018-01-10 20:29:42 +0100

    fill runApplication method for tests purposes

--- application/RootController.java
+++ application/RootController.java
@@ -2,6 +2,8 @@ package application;
 
 import users.UsersDAO;
 // import users.UserCtrl;
+import users.StudentModel;
+
 
 public class RootController
 {
@@ -19,6 +21,10 @@ public class RootController
     {
         view.displayIntro();
         view.displayLoginScreen();
+        StudentModel student = new StudentModel("Jarek", "Kucharczyk", "123");
+        String message = student.getUserFirstName()+", role: " + student.getUserRole();
+        view.displayMessage(message);
+        view.handlePause();
         view.displayLogoutScreen();
         view.displayOutro();
     }
