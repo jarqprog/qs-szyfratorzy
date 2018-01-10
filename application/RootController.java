43e722b1f19bbf7d9386f591bd2694e84912020b
jarqprog <jarqprog@gmail.com>
2018-01-10 19:34:13 +0100

    add constructor and tmp runApplication method in RootController class

--- application/RootController.java
+++ application/RootController.java
@@ -1,7 +1,28 @@
 package application;
 
-public class RootController{
-    public static void main(String[] args){
+import users.UsersDAO;
+// import users.UserCtrl;
 
+public class RootController
+{
+
+    private AbstractDAO dao;
+    private RootView view;
+    // private UserCtrl loggedUser;
+    public RootController()
+    {
+        dao = new UsersDAO();
+        view = new RootView();
+    }
+
+    public void runApplication()
+    {
+        view.displayIntro();
+        view.handlePause();
+        view.displayLoginScreen();
+        view.handlePause();
+        view.displayLogoutScreen();
+        view.handlePause();
+        view.displayOutro();
     }
 }
