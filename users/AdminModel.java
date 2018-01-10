225fedb569b93b907dd1653988c76c03d17705a0
Adrian Bala <aadrianbala@gmail.com>
2018-01-10 13:47:20 +0100

    Add AdminModel class in package users

---
+++ users/AdminModel.java
@@ -0,0 +1,9 @@
+package users;
+
+public class AdminModel extends UserModel {
+    
+    public AdminModel(int ID, String fname, String lname, String email, String password)
+    {
+    super(ID, firstName, lastName, email, password);
+    }
+}
