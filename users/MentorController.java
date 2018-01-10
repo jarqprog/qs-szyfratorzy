7465475198398993faf04e87f26504fac5a4bc54
Adrian Bala <aadrianbala@gmail.com>
2018-01-10 20:08:22 +0100

    Update create artefact method and add main method in MentorController class

--- users/MentorController.java
+++ users/MentorController.java
@@ -1,35 +1,37 @@
 package users;
 
-public class MentorController extends AbstractUserController {
+import item.*;
+import java.util.Scanner;
+
+public class MentorController {
     MentorView mentorView;
-    // MentorDao mentorDao;
-    // StudentDao studentDao;
-    // QuestDao questDao;
-    // ArtefactDao artefactDao;
+
+    public static void main(String[] args) {
+        MentorController a = new MentorController();
+        a.createStudent();
+        a.createArtifact();
+    }
 
     public MentorController() {
         mentorView = new MentorView();
-        // mentorDao = new MentorDao();
-        // studentDao = new StudentDao();
-        // questDao = new QuestDao();
-        // artefactDao = new ArtefactDao();
+
     }
 
     public void createStudent() {
-        String firstName = mentorView.getInput("Enter a first name:");
-        String lastName = mentorView.getInput("Enter a last name:");
-        String password = mentorView.getInput("Enter a password:");
+        String firstName = mentorView.getUserInput("Enter a first name:");
+        String lastName = mentorView.getUserInput("Enter a last name:");
+        String password = mentorView.getUserInput("Enter a password:");
         StudentModel newStudent = new StudentModel(firstName, lastName, password);
-        // studentDao.exportData(newStudent);
+        System.out.println(newStudent);
 
     }
 
     public void createArtifact() {
-        char itemType = getInput();
-        String itemName = getInput("Enter an item name:");
-        String itemDescription = getInput("Enter an item name:");
-        int price = getPrice();
-
-        new ArtefactModel(itemType, itemName, itemDescription, price);
+        char itemType = mentorView.getUserInput("Enter an item type: ").charAt(0);
+        String itemName = mentorView.getUserInput("Enter an item name:");
+        String itemDescription = mentorView.getUserInput("Enter an item description:");
+        int price = mentorView.getPrice();
+        ArtefactModel artefact = new ArtefactModel(itemType, itemName, itemDescription, price);
+        System.out.println(artefact);
     }
 }
