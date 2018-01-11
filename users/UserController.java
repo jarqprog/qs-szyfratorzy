c8d34906852dbad175e3a46a107bb1da8ddb18eb
PiotrGry <piotrgryzlo@gmail.com>
2018-01-11 10:28:17 +0100

    Add UserController class

---
+++ users/UserController.java
@@ -0,0 +1,11 @@
+package users;
+import application.AbstractDAO;
+import application.AbstractView;
+
+public class abstract UserController{
+
+    AbstractDAO DAO;
+    AbstractView view;
+
+    public abstract void handleMainMenu()
+}
