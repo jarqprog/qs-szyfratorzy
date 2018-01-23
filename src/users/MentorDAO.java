25d047b1f142d1c4849c3fe36bb7d85dd6ec56a5
jarqprog <jarqprog@gmail.com>
2018-01-23 19:59:29 +0100

    add UsersDAO (Mentor, Admin, Student) with basic methods

--- src/users/MentorDAO.java
+++ src/users/MentorDAO.java
@@ -1,6 +1,24 @@
 package users;
 
+import java.util.ArrayList;
+import java.util.List;
+import application.Table;
 
 public class MentorDAO extends UsersDAO {
+
+
+    private final String DEFAULT_TABLE = Table.MENTORS.getName();
+
+    public List<MentorModel> getAllMentors(){
+        List<MentorModel> mentors = new ArrayList<MentorModel>();
+        ///
+        return mentors;
+    }
+
+    public MentorModel getMentorById(int id){
+
+        // temporary
+        return new MentorModel(201, "Marian", "Nowak", "nowak@cc.pl", "12321", 'a');
+    }
 
 }
