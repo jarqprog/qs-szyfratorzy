5148b8bbad57c1e7dccd2bed7f7c27e2911a986c
Adrian Bala <aadrianbala@gmail.com>
2018-01-24 21:52:07 +0100

    Implement GroupDAO class in school package

--- src/school/GroupDAO.java
+++ src/school/GroupDAO.java
@@ -10,28 +10,47 @@ public class GroupDAO {
 
     private final String DEFAULT_TABLE = Table.GROUPS.getName();
 
-    public List<GroupModel> getObjects(String query) {
+    private final Integer ID_INDEX = 0;
+    private final Integer NAME_INDEX = 1;
+    private final Integer STUD_LIST_INDEX = 2;
+
+    public List<GroupModel> getObjects(List<String[]> dataCollection) {
 
         List<GroupModel> groups = new ArrayList<GroupModel>();
 
-        /// impl
+        for (String [] record : dataCollection) {
+            GroupModel group = getObject(record);
+            groups.add(group);
+        }
 
         return groups;
     }
 
-    public GroupModel getObject(String query){
+    public GroupModel getObject(String [] record){
 
-        // temporary impl
-        return new GroupModel(200, "a", new ArrayList<StudentModel>());
+        int id = Integer.parseInt(record[ID_INDEX]);
+        String name = record[NAME_INDEX];
+        // temp
+        List<StudentModel> students = new ArrayList<StudentModel>();
+
+        return new GroupModel(id, name, students);
     }
 
     public void saveObject(GroupModel group){
 
-        //
+        String group_id = String.valueOf(group.getId());
+        String name = group.getName();
+        // dodac List<StudentModel>
+        String query = String.format("UPDATE %s SET name=%s " +
+                "WHERE id=%s;", DEFAULT_TABLE, name, group_id);
+        DbManagerDAO dao = new DbManagerDAO();
+        dao.inputData(query);
     }
 
     public void saveObjects(List<GroupModel> groups){
 
-        //
+        for(GroupModel group : groups) {
+            saveObject(group);
+        }
     }
 }
