d45ef79d7b04bf24c6dab4ac04b58af428747121
PiotrGry <piotrgryzlo@gmail.com>
2018-01-24 19:31:57 +0100

    Refactor ArtefactDao in item package

--- src/item/ArtifactDAO.java
+++ src/item/ArtifactDAO.java
@@ -13,11 +13,11 @@ public class ArtifactDAO {
     private final String DEFAULT_TABLE = Table.ARTIFACTS.getName();
     private DbManagerDAO daoManager = new DbManagerDAO();
 
-    protected final static int ID_INDEX = 0;
-    protected final static int TYPE_INDEX = 1;
-    protected final static int NAME_INDEX = 2;
-    protected final static int DESCRIPTION_INDEX = 3;
-    protected final static int PRICE_INDEX = 4;
+    private final static int ID_INDEX = 0;
+    private final static int TYPE_INDEX = 1;
+    private final static int NAME_INDEX = 2;
+    private final static int DESCRIPTION_INDEX = 3;
+    private final static int PRICE_INDEX = 4;
 
     public List<ArtifactModel> getObjects(List<String[]> dataCollection) {
 
@@ -32,42 +32,12 @@ public class ArtifactDAO {
 
     public ArtifactModel getObject(String[] record) {
         int id = Integer.parseInt(record[ID_INDEX]);
-        String itemType = record[TYPE_INDEX];
+        char itemType = record[TYPE_INDEX].charAt(0);
         String itemName = record[NAME_INDEX];
         String itemDescription = record[DESCRIPTION_INDEX];
         int price = Integer.parseInt(record[PRICE_INDEX]);
 
-
         return new ArtifactModel(id, itemType, itemName, itemDescription, price);
-    }
-
-    public ArtifactModel getObject(String query) {
-
-        String sqlStatement = String.format("SELECT * FROM %s WHERE %s", DEFAULT_TABLE, query);
-        String [] record = daoManager.getData(sqlStatement);
-
-            int id = Integer.parseInt(record[ID_INDEX]);
-            char itemType = record[TYPE_INDEX].charAt(0);
-            String itemName = record[NAME_INDEX];
-            String itemDescription = record[DESCRIPTION_INDEX];
-            int price = Integer.parseInt(record[PRICE_INDEX]);
-
-
-        return new ArtifactModel(id, itemType, itemName, itemDescription, price);
-    }
-
-    public List<ArtifactModel> getObjects(String query) {
-
-        String sqlStatement = String.format("SELECT * FROM %s WHERE %s", DEFAULT_TABLE, query);
-        List<String []> artifacts = daoManager.getData(sqlStatement);
-        ArrayList<ArtifactModel> inventory = new ArrayList<ArtifactModel>();
-
-        for (String [] record : artifacts) {
-            ArtifactModel artifact = getObject(record);
-            inventory.add(artifact);
-        }
-
-        return inventory;
     }
 
     public void saveObject(ArtifactModel artifact) {
