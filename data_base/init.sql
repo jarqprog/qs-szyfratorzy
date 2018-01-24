01b41c1e0e27be0b060ca8286e4632113190fb83
Robstepa <zb.arthur@gmail.com>
2018-01-24 13:38:34 +0100

    Add table creating

--- data_base/init.sql
+++ data_base/init.sql
@@ -1,11 +1,22 @@
-PRAGMA FOREIGN_KEYS = OFF;
+PRAGMA  FOREIGN_KEYS = ON;
+
 BEGIN TRANSACTION;
+
 CREATE TABLE IF NOT EXISTS mentors(
   id integer PRIMARY KEY AUTOINCREMENT,
   first_name text,
   last_name text,
   email text,
   password text);
+
+CREATE TABLE IF NOT EXISTS groups(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  name text UNIQUE);
+ 
+CREATE TABLE  IF NOT EXISTS teams(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  name text UNIQUE);
+
 CREATE TABLE IF NOT EXISTS students(
   id integer PRIMARY KEY AUTOINCREMENT,
   first_name text,
@@ -13,15 +24,57 @@ CREATE TABLE IF NOT EXISTS students(
   email text,
   password text,
   team_id integer,
+  FOREIGN KEY (team_id) REFERENCES teams(id),
   group_id integer,
+  FOREIGN KEY (group_id) REFERENCES groups(id),
   wallet integer,
   expirence integer);
+
 CREATE TABLE IF NOT EXISTS admins(
   id integer PRIMARY KEY AUTOINCREMENT,
   first_name text,
   last_name text,
   email text,
   password text);
+
+CREATE TABLE IF NOT EXISTS artifacts(
+  id integer PRIMARY KEY AUTOINCREMENT ,
+  name text,
+  type text,
+  description text,
+  price integer);
+
+CREATE TABLE IF NOT EXISTS quests(
+  id integer PRIMARY KEY AUTOINCREMENT ,
+  name text,
+  type text,
+  description text,
+  reward integer);
+  
+CREATE TABLE IF NOT EXISTS teams_transactions(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  team_id integer,
+  FOREIGN KEY (team_id) REFERENCES teams(id),
+  artefact_id integer,
+  FOREIGN KEY (artefact_id) REFERENCES artifacts(id),
+  date text);
+  
+CREATE TABLE IF NOT EXISTS students_transactions(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  student_id integer,
+  FOREIGN KEY (student_id) REFERENCES students(id),
+  artefact_id integer,
+  FOREIGN KEY (artefact_id) REFERENCES artifacts(id),
+  date text);
+  
+CREATE TABLE IF NOT EXISTS students_artifacts(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  student_id integer,
+  FOREIGN KEY (student_id) REFERENCES students(id),
+  artefact_id integer,
+  FOREIGN KEY (artefact_id) REFERENCES artifacts(id));
+  
 INSERT INTO admins(first_name, last_name, email, password)
 VALUES(admin, admin, admin@email.com, admin);
+
 COMMIT;
