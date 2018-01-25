39151074bdf4608d6e485338ec2857be7b5cb513
Robstepa <zb.arthur@gmail.com>
2018-01-25 18:56:51 +0100

    Implement TeamDao class

--- src/school/TeamDAO.java
+++ src/school/TeamDAO.java
@@ -8,30 +8,79 @@ import users.StudentModel;
 
 public class TeamDAO {
 
+    private DbManagerDAO dao;
+
     private final String DEFAULT_TABLE = Table.GROUPS.getName();
 
-    public List<TeamModel> getObjects(String query) {
+    private final Integer ID_INDEX = 0;
+    private final Integer NAME_INDEX = 1;
+
+    private int teamId;
+    private String name;
+
+    public List<TeamModel> getManyObjects(List<String[]> dataCollection) {
 
         List<TeamModel> teams = new ArrayList<TeamModel>();
-
-        /// impl
-
+        for (String[] record : dataCollection) {
+            TeamModel team = getOneObject(record);
+            teams.add(team);
+        }
         return teams;
     }
 
-    public TeamModel getObject(String query){
-
-        // temporary impl
-        return new TeamModel(200, "a", new ArrayList<StudentModel>());
+    public List<TeamModel> getManyObjects(String query) {
+        DbManagerDAO dao = new DbManagerDAO();
+        List<String[]> dataCollection = dao.getData(query);
+        List<TeamModel> teams = new ArrayList<TeamModel>();
+        for (String[] record : dataCollection) {
+            TeamModel team = getOneObject(record);
+            teams.add(team);
+        }
+        return teams;
     }
 
-    public void saveObject(TeamModel team){
+    public TeamModel getOneObject(String[] teamData) {
+        
+        name = teamData[NAME_INDEX];
 
-        //
+        return new TeamModel(name);
+    }
+
+    public TeamModel getOneObject(String query) {
+        dao = new DbManagerDAO();
+        String[] teamData = dao.getData(query).get(0);
+        name = teamData[NAME_INDEX];
+
+        return new TeamModel(name);
+    }
+
+    public void saveObject(TeamModel team) {
+        String teamId = String.valueOf(team.getId());
+        name = team.getName();
+
+        String query;
+        if(teamId.equals("-1")){
+
+            query = String.format(
+                            "INSERT INTO %s " +
+                            "VALUES(null, '%s');",
+                    DEFAULT_TABLE, name);
+
+        } else{
+
+            query = String.format(
+                            "UPDATE %s SET name='%s'" +
+                            "WHERE id=%s;", DEFAULT_TABLE, name);
+        }
+
+        dao = new DbManagerDAO();
+        dao.inputData(query);
     }
 
     public void saveObjects(List<TeamModel> teams){
 
-        //
+        for(TeamModel team : teams) {
+            saveObject(team);
+        }
     }
 }
