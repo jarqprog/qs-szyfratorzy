06af37f2089cad3523c414c16dd4b9e3175f0985
jarqprog <jarqprog@gmail.com>
2018-01-25 09:50:27 +0100

    add database files - updated

--- data_base/init.sql
+++ data_base/init.sql
@@ -1,25 +1,25 @@
-PRAGMA  FOREIGN_KEYS = ON;
+PRAGMA foreign_keys=ON;
 BEGIN TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS groups(
-  group_id integer PRIMARY KEY AUTOINCREMENT,
+  id integer PRIMARY KEY AUTOINCREMENT,
   name text UNIQUE);
 
 CREATE TABLE  IF NOT EXISTS teams(
-  team_id integer PRIMARY KEY AUTOINCREMENT,
+  id integer PRIMARY KEY AUTOINCREMENT,
   name text UNIQUE);
 
 CREATE TABLE IF NOT EXISTS mentors(
-  mentor_id integer PRIMARY KEY AUTOINCREMENT,
+  id integer PRIMARY KEY AUTOINCREMENT,
   first_name text,
   last_name text,
   email text,
   password text,
   group_id integer,
-  FOREIGN KEY (group_id) REFERENCES groups(group_id));
+  FOREIGN KEY (group_id) REFERENCES groups(id));
 
 CREATE TABLE IF NOT EXISTS students(
-  student_id integer PRIMARY KEY AUTOINCREMENT,
+  id integer PRIMARY KEY AUTOINCREMENT,
   first_name text,
   last_name text,
   email text,
@@ -29,40 +29,40 @@ CREATE TABLE IF NOT EXISTS students(
   attendance float,
   team_id integer,
 	group_id integer,
-	FOREIGN KEY (team_id) REFERENCES teams(team_id),
-	FOREIGN KEY (group_id) REFERENCES groups(group_id));
+	FOREIGN KEY (team_id) REFERENCES teams(id),
+	FOREIGN KEY (group_id) REFERENCES groups(id));
 
 CREATE TABLE IF NOT EXISTS admins(
-  admin_id integer PRIMARY KEY AUTOINCREMENT,
+  id integer PRIMARY KEY AUTOINCREMENT,
   first_name text,
   last_name text,
   email text,
   password text);
 
 CREATE TABLE IF NOT EXISTS artifacts(
-  artifact_id integer PRIMARY KEY AUTOINCREMENT ,
+  id integer PRIMARY KEY AUTOINCREMENT ,
   name text,
   type text,
   description text,
   price integer);
 
 CREATE TABLE IF NOT EXISTS quests(
-  quest_id integer PRIMARY KEY AUTOINCREMENT ,
+  id integer PRIMARY KEY AUTOINCREMENT ,
   name text,
   type text,
   description text,
   reward integer);
 
 CREATE TABLE IF NOT EXISTS teams_transactions(
-  team_transaction_id integer PRIMARY KEY AUTOINCREMENT,
+  id integer PRIMARY KEY AUTOINCREMENT,
   team_id integer,
   artefact_id integer,
   date text,
-  FOREIGN KEY (team_id) REFERENCES teams(team_id),
-  FOREIGN KEY (artefact_id) REFERENCES artifacts(artefact_id));
+  FOREIGN KEY (team_id) REFERENCES teams(id),
+  FOREIGN KEY (artefact_id) REFERENCES artifacts(id));
 
 CREATE TABLE IF NOT EXISTS students_transactions(
-  student_transaction_id integer PRIMARY KEY AUTOINCREMENT,
+  id integer PRIMARY KEY AUTOINCREMENT,
   student_id integer,
   artefact_id integer,
   date text,
@@ -70,17 +70,19 @@ CREATE TABLE IF NOT EXISTS students_transactions(
   FOREIGN KEY (student_id) REFERENCES students(id));
 
 CREATE TABLE IF NOT EXISTS students_artifacts(
-  student_artifact_id integer PRIMARY KEY AUTOINCREMENT,
+  id integer PRIMARY KEY AUTOINCREMENT,
   student_id integer,
   artefact_id integer,
-  FOREIGN KEY (artefact_id) REFERENCES artifacts(artifact_id),
-  FOREIGN KEY (student_id) REFERENCES students(student_id));
+  FOREIGN KEY (artefact_id) REFERENCES artifacts(id),
+  FOREIGN KEY (student_id) REFERENCES students(id));
 
 -- add data
 
-  INSERT OR IGNORE INTO admins VALUES(1,'admin','admin','admin@email.com','admin');
   INSERT OR IGNORE INTO groups VALUES(1,'undefined');
   INSERT OR IGNORE INTO teams VALUES(1,'undefined');
+  INSERT OR IGNORE INTO admins VALUES(1,'admin','admin','admin@email.com','admin');
+  INSERT OR IGNORE INTO students VALUES(100,'Marcinek','Stasik','lolek@gmail.com','12321', 20, 30, 40, 1, 1);
+
 
 DELETE FROM sqlite_sequence;
 
