51e297adb4ab66542706567328f2bc96b23b2853
jarqprog <jarqprog@gmail.com>
2018-01-26 07:27:04 +0100

    refactor, implement Quest features in MentorController

--- src/users/MentorController.java
+++ src/users/MentorController.java
@@ -2,19 +2,21 @@ package users;
 
 import item.ArtifactDAO;
 import item.ArtifactModel;
+import item.QuestDAO;
+import item.QuestModel;
+
 import java.util.List;
 
 public class MentorController extends UserController{
     MentorView view;
     MentorModel mentor;
     MentorDAO dao;
-    ArtifactDAO artifactDao;
 
     public MentorController(MentorModel mentorModel){
         mentor = mentorModel;
         view = new MentorView();
         dao = new MentorDAO();
-        artifactDao = new ArtifactDAO();
+
     }
 
     public void handleMainMenu(){
@@ -64,27 +66,41 @@ public class MentorController extends UserController{
     }
 
     public void createQuest() {
-        executeNotImplementedInfo();
+        String name = view.getUserInput("Enter quest name: ");
+        String description = view.getUserInput("Enter quest description: ");
+        int reward = view.getValue();
+        QuestModel quest = new QuestModel(name, description, reward);
+        view.displayMessage("You've created new something new:\n" + quest);
     }
 
     public void editQuest() {
-        executeNotImplementedInfo();
+
+        List<QuestModel> quests = getQuests();
+        view.displayElementsOfCollection(prepareQuestsToDisplay(quests));
+        String input = view.getUserInput("Enter id of quest which you would like to edit: ");
+        for(QuestModel quest : quests) {
+            if(String.valueOf(quest.getId()).equals(input)) {
+                handleQuestEditMenu(quest);
+                break;
+            }
+        }
+
     }
 
     public void createStudent(){
-        String firstName = view.getUserInput("Enter a first name: ");
-        String lastName = view.getUserInput("Enter a last name: ");
-        String password = view.getUserInput("Enter a password: ");
+        String firstName = view.getUserInput("Enter first name: ");
+        String lastName = view.getUserInput("Enter last name: ");
+        String password = view.getUserInput("Enter password: ");
         StudentModel newStudent = new StudentModel(firstName, lastName, password);
         view.displayMessage("Student created successfully! \n" + newStudent.toString());
     }
 
     public void createArtifact(){
-        String name = view.getUserInput("Enter an artifact name: ");
-        String description = view.getUserInput("Enter an item description: ");
-        int price = view.getPrice();
+        String name = view.getUserInput("Enter artifact name: ");
+        String description = view.getUserInput("Enter artifact description: ");
+        int price = view.getValue();
         ArtifactModel artifact = new ArtifactModel(name, description, price);
-        view.displayMessage("Artifact " + artifact + " created successfully!");
+        view.displayMessage("You've created new something new:\n" + artifact);
         }
 
     public void editArtifact(){
@@ -124,7 +140,7 @@ public class MentorController extends UserController{
                     artifact.setDescription(view.getUserInput("Enter new artifact description: "));
                     break;
                 case "4":
-                    artifact.setPrice(view.getPrice());
+                    artifact.setPrice(view.getValue());
                     break;
                 case "0":
                     ArtifactDAO dao = new ArtifactDAO();
@@ -135,6 +151,45 @@ public class MentorController extends UserController{
             if(! isDone){
                view.displayMessage(artifact.toString());
                view.handlePause();
+            }
+        }
+    }
+
+    private void handleQuestEditMenu(QuestModel quest){
+        boolean isDone = false;
+        while(! isDone){
+            String userChoice = "";
+            String[] correctChoices = {"1", "2", "3", "4", "0"};
+            Boolean isChoiceReady = false;
+            while(! isChoiceReady){
+                view.clearScreen();
+                view.displayQuestMenu();
+                userChoice = view.getUserInput("Select an option: ");
+                isChoiceReady = checkIfElementInArray(correctChoices, userChoice);
+            }
+            view.clearScreen();
+            switch(userChoice){
+                case "1":
+                    quest.setName(view.getUserInput("Enter new quest name: "));
+                    break;
+                case "2":
+                    quest.setType(view.getUserInput("Enter new quest type: ").charAt(0));
+                    break;
+                case "3":
+                    quest.setDescription(view.getUserInput("Enter new quest description: "));
+                    break;
+                case "4":
+                    quest.setReward(view.getValue());
+                    break;
+                case "0":
+                    QuestDAO dao = new QuestDAO();
+                    dao.saveObject(quest);
+                    isDone = true;
+                    break;
+            }
+            if(! isDone){
+                view.displayMessage(quest.toString());
+                view.handlePause();
             }
         }
     }
