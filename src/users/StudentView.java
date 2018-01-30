d0e40024b88a18535da5dbaa44c7e574c41f64ef
Adrian Bala <aadrianbala@gmail.com>
2018-01-30 17:18:56 +0100

    Add displayInventory method and update display menu in Student view class

--- src/users/StudentView.java
+++ src/users/StudentView.java
@@ -1,6 +1,9 @@
 package users;
 
 import application.AbstractView;
+import item.ArtifactModel;
+
+import java.util.List;
 
 public class StudentView extends AbstractView{
 
@@ -14,10 +17,23 @@ public class StudentView extends AbstractView{
         displayMessage(Integer.toString(experience));
     }
 
+    public void displayInventory(List<ArtifactModel> inventory) {
+        if(inventory.size() < 1) {
+            displayMessage("Your inventory is empty");
+        } else {
+            displayMessage("Your Inventory: \n");
+            for(ArtifactModel artifact : inventory) {
+            System.out.println(artifact);
+            }
+        }
+    }
+
     public void displayMenu() {
         String[] options = {"      *** Student's Menu ***     ",
                             "[1] display wallet",
                             "[2] display level of experience",
+                            "[3] shop online",
+                            "[4] display inventory",
                             "[0] exit"};
 
         for(String element : options) {
