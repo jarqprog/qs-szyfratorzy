a536c9320e185ace15ab1212caa5d3877f92f141
jarqprog <jarqprog@gmail.com>
2018-01-30 19:07:29 +0100

    add methods getCurrentIdCollection(), getNewId() - to repair id mechanic in FactoryDAO class

--- src/application/FactoryDAO.java
+++ src/application/FactoryDAO.java
@@ -1,7 +1,7 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.List;
 
 public abstract class FactoryDAO implements CreatableDAO {
@@ -9,12 +9,12 @@ public abstract class FactoryDAO implements CreatableDAO {
     protected String DEFAULT_TABLE;
     protected DbManagerDAO dao;
 
-    public Object getObjectById(int id){
+    public Object getObjectById(int id) {
         String query = "Select * from " + DEFAULT_TABLE + " WHERE id=" + id + ";";
         return getOneObject(query);
     }
 
-    public List<Object> getObjects(){
+    public List<Object> getObjects() {
         String query = "Select * from " + DEFAULT_TABLE + ";";
         return getAllObjects(query);
     }
@@ -34,4 +34,25 @@ public abstract class FactoryDAO implements CreatableDAO {
 
     public abstract Object getOneObject(String query);
 
+    protected String[] getCurrentIdCollection() {
+        final String query = String.format("SELECT id FROM %s;", this.DEFAULT_TABLE);
+        int idIndex = 0;
+        DbManagerDAO dao = new DbManagerDAO();
+        List<String[]> currentIdsCollection = dao.getData(query);
+        String[] currentIds = new String[currentIdsCollection.size()];
+        for (int i = 0; i < currentIdsCollection.size(); i++) {
+            currentIds[i] = currentIdsCollection.get(i)[idIndex];
+        }
+        return currentIds;
+    }
+
+    protected String getNewId(String[] oldIdCollection, String[] newIdCollection){
+        for(String id : newIdCollection){
+            // compare collections: old collection doesn't contain new id:
+            if(! Arrays.asList(oldIdCollection).contains(id)){
+                return id;
+            }
+        }
+        return null;
+    }
 }
