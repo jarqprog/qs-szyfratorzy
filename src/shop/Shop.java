c05fe5dbd3357fd4ee3e8f35a3bc82479bd58dfc
Adrian Bala <aadrianbala@gmail.com>
2018-01-30 17:12:44 +0100

    Implement shop class in shop package

--- src/shop/Shop.java
+++ src/shop/Shop.java
@@ -1,5 +1,19 @@
 package shop;
 
+import item.ArtifactModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Shop{
+    List<ArtifactModel> store;
+
+    public Shop() {
+        store = new ArrayList<>();
+        }
+
+    public List getStore() { return store;}
+
+    public void setStore(List<ArtifactModel> store) {this.store = store;}
 
 }
