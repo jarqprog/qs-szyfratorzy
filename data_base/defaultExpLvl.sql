6750fdd2a2b097bcdbbae187ab1d28b6dc1def7a
jarqprog <jarqprog@gmail.com>
2018-02-03 15:47:08 +0100

    update sql file - to clear old experience level data in DB

--- data_base/defaultExpLvl.sql
+++ data_base/defaultExpLvl.sql
@@ -1,14 +1,11 @@
 PRAGMA foreign_keys=ON;
 BEGIN TRANSACTION;
-
 DELETE FROM experience_levels;
-
   INSERT OR IGNORE INTO experience_levels VALUES(1,'novice',0);
   INSERT OR IGNORE INTO experience_levels VALUES(2,'apprentice',100);
   INSERT OR IGNORE INTO experience_levels VALUES(3,'junior programmer',1000);
   INSERT OR IGNORE INTO experience_levels VALUES(4,'programmer',10000);
   INSERT OR IGNORE INTO experience_levels VALUES(5,'senior programmer',100000);
   INSERT OR IGNORE INTO experience_levels VALUES(6,'almost mentor',1000000);
-
 DELETE FROM sqlite_sequence;
 COMMIT;
