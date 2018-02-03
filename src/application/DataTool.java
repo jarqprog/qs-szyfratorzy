becce2c96798ee13724b24d104768b1509c5fdd1
jarqprog <jarqprog@gmail.com>
2018-02-03 15:46:16 +0100

    add DataTool class with static methods to operate on data

---
+++ src/application/DataTool.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
+import java.util.Map.Entry;
+
+public class DataTool {
+
+    public static Boolean checkIfElementInArray(String[] array, String element) {
+        for(String item : array){
+            if(item.equals(element)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static LinkedHashMap<String,Integer> sortMapByValue(Map<String,Integer> map){
+        return map.entrySet().stream().sorted(Map.Entry.comparingByValue())
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+    }
+
+}
