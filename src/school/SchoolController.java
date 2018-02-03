d757a7a5b57f6ad694fe0a6970f32f0d8659104e
jarqprog <jarqprog@gmail.com>
2018-02-03 13:01:55 +0100

    add new SchoolController class with basic methods

---
+++ src/school/SchoolController.java
@@ -0,0 +1,29 @@
+package school;
+
+import java.util.List;
+
+public class SchoolController {
+
+    private SchoolModel school;
+
+    public SchoolController(SchoolModel school){
+        this.school = school;
+    }
+
+    public SchoolController(){
+        this.school = new SchoolModel();
+    }
+
+    public List<GroupModel> getGroups(){
+        return school.getGroups();
+    }
+
+    public List<TeamModel> getTeams(){
+        return school.getTeams();
+    }
+
+    public SchoolModel getSchool(){
+        return school;
+    }
+
+}
