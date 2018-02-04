bf7b062edb56a7d2610cac3453876d296b7e1b7f
jarqprog <jarqprog@gmail.com>
2018-02-04 14:51:18 +0100

    refactor

--- src/school/SchoolView.java
+++ src/school/SchoolView.java
@@ -8,14 +8,13 @@ public class SchoolView extends AbstractView{
         String[] options = {"      *** Experience level manager ***     \n",
                 " [1] display experience levels",
                 " [2] restore default experience levels",
-                " [3] modify experience level",
-                " [4] create new experience level",
-                " [5] delete experience level",
+                " [3] modify an existing or create new experience level",
+                " [4] delete experience level",
+                " [5] delete ALL experience levels",
                 " [0] exit manager\n"};
 
         for(String element : options) {
             System.out.println(element);
         }
     }
-
 }
