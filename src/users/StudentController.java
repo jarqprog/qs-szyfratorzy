d2c019a09f169d64b125110ea013e57de85d98c2
Adrian Bala <aadrianbala@gmail.com>
2018-02-06 16:05:33 +0100

    Start adding update student inventory to student controller

--- src/users/StudentController.java
+++ src/users/StudentController.java
@@ -48,6 +48,13 @@ public class StudentController extends UserController{
         shopDAO.saveInventory(student.getId(), student.getInventory());
     }
 
+//    private void updateTeamInventory() {
+//        shopDAO = new ShopDAO();
+//        List<String []> artifacts =  shopDAO.findTeamArtifacts(student.getTeam().getId());
+//        student.getTeam().setInventory(shopDAO.loadInventory(artifacts));
+//        shopDAO.saveInventory(student.getId(), student.getInventory());
+//    }
+
     private void executeShopping() {
         ShopModel shop = new ShopModel();
         ShopController controller = new ShopController(shop, student);
