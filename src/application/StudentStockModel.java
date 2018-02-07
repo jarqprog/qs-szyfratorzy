a3b2523dc629da1a8363b1a224078a782a05b2ae
PiotrGry <piotrgryzlo@gmail.com>
2018-02-07 16:39:07 +0100

    add methods in StudentStockModel

--- src/application/StudentStockModel.java
+++ src/application/StudentStockModel.java
@@ -1,4 +1,56 @@
 package application;
 
-public class StudentStockModel {
+import item.ItemModel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class StudentStockModel {
+    protected int id;
+    protected Map<ItemModel, ?> stock;
+
+    public StudentStockModel(int id) {
+        this.id = id;
+        stock = new HashMap();
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setStock(Map stock) {
+        this.stock = stock;
+    }
+
+    public ItemModel getItem(int itemId) {
+        Set<ItemModel> items = stock.keySet();
+        ItemModel item = null;
+        for (ItemModel element : items) {
+            if (itemId == element.getId()) {
+                item = element;
+                break;
+            }
+        }
+        return item;
+    }
+
+    public boolean containsItem(ItemModel item) {
+        boolean elementFound = false;
+        for (ItemModel element : stock.keySet()) {
+            if (element.compare(item)) {
+                elementFound = true;
+                break;
+            }
+        }
+        return elementFound;
+    }
+
+
+    public abstract Map getStock();
+
+    public abstract void setStock();     //use DAO
+
+    public abstract void addItem();
+
 }
