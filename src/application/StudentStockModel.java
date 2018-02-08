9051302fa9977140aebfc99def34aebc35c52770
jarqprog <jarqprog@gmail.com>
2018-02-08 15:16:00 +0100

    implement StudentStockModel

---
+++ src/application/StudentStockModel.java
@@ -0,0 +1,67 @@
+package application;
+
+
+import item.ItemModel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.*;
+
+
+public class StudentStockModel {
+
+    protected int studentId;
+    protected Map<? extends ItemModel, ?> stock;
+
+    public StudentStockModel(int studentId) {
+        this.studentId = studentId;
+        stock = new HashMap<>();
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public <K extends ItemModel, V> void setStock(Map<K,V> stock) {
+        this.stock = stock;
+    }
+
+    public <T extends ItemModel> T getItem(int itemId) {
+        for (Map.Entry<?,?> entry : stock.entrySet()) {
+            @SuppressWarnings("unchecked")
+            T inStockItem = (T) entry.getKey();
+            if (inStockItem.getId() == itemId) {
+                return inStockItem;
+            }
+        }
+        return null;
+    }
+
+    public <T extends ItemModel> boolean containsItem(T item) {
+        for (Map.Entry<?,?> entry : stock.entrySet()) {
+            @SuppressWarnings("unchecked")
+            T inStockItem = (T) entry.getKey();
+            if (inStockItem.getName().equals(item.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isEmpty(){
+        return stock.size() > 0;
+    }
+
+
+    //public abstract Map getStock();
+
+    //public abstract void setStock();
+
+
+
+}
