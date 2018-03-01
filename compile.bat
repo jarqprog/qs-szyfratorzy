7b8f18385401dd154eb080a10ebe7126e5156c35
jarqprog <jarqprog@gmail.com>
2018-03-01 19:14:37 +0100

    change compile.bat file (add new packages)

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-javac -d bin src/application/*.java src/item/*.java src/school/*.java src/shop/*.java src/users/*.java
+javac -d bin src/application/*.java src/controllers/*.java src/dao/*.java src/enums/*.java src/exceptions/*.java src/factory/*.java src/model/*.java src/tools/*.java src/view/*.java
