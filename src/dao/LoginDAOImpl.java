91c797211d42c3f767f5263fef84b85a8c57f557
jarqprog <jarqprog@gmail.com>
2018-03-01 19:26:11 +0100

    add interface LoginDAO, change LoginDAO to LoginDAOImpl, change static method to instance method

---
+++ src/dao/LoginDAOImpl.java
@@ -0,0 +1,63 @@
+package dao;
+
+import controllers.*;
+import enums.Table;
+import exceptions.LoginFailure;
+import factory.ConnectionFactory;
+import factory.UserControllerFactory;
+import managers.DbManagerDAO;
+import model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginDAOImpl implements LoginDAO {
+
+    public UserController getUserControllerByLoginAndPassword(String login, String password) throws LoginFailure {
+        User user;
+        DbManagerDAO dao = new DbManagerDAO();
+        Connection connection = ConnectionFactory.getConnection();
+        String [] usersTables = {Table.ADMINS.getName(), Table.MENTORS.getName(), Table.STUDENTS.getName()};
+        String statement;
+        for(String table : usersTables) {
+            statement = String.format("SELECT * FROM %s WHERE first_name || id =? AND password=?", table);
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement(statement);
+                preparedStatement.setString(1, login);
+                preparedStatement.setString(2, password);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                if (resultSet.isBeforeFirst()) {
+                    String[] userData = dao.getObjectData(resultSet);
+                    user = extractUser(userData, table);
+                    if (user != null) {
+                        return UserControllerFactory.getController(user);
+                    }
+                }
+            } catch (SQLException e) {
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            }
+        }
+        throw new LoginFailure();
+    }
+
+    private User extractUser(String[] userData, String table) {
+        User user = null;
+        switch (table) {
+            case ("admins"):
+                AdminDAO adDao = new AdminDAO();
+                user = adDao.getOneObject(userData);
+                break;
+            case ("mentors"):
+                MentorDAO meDao = new MentorDAO();
+                user = meDao.getOneObject(userData);
+                break;
+            case ("students"):
+                StudentDAO stDao = new StudentDAO();
+                user = stDao.getOneObject(userData);
+                break;
+        }
+        return user;
+    }
+}
