fb1fe0f205bd8d486e8f4170fdbfedb2d6b308f1
jarqprog <jarqprog@gmail.com>
2018-03-02 08:50:34 +0100

    add getConnection() to DbManager

--- src/managers/DbManager.java
+++ src/managers/DbManager.java
@@ -2,9 +2,12 @@ package managers;
 
 import exceptions.DatabaseFailure;
 
+import java.sql.Connection;
+
 public interface DbManager {
 
     void prepareDatabase();
+    Connection getConnection();
     void closeConnection();
     void updateDatabase(String sqlScriptPath) throws DatabaseFailure;
 }
