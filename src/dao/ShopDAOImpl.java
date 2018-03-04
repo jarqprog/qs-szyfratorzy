48ee186479ae67ab3882a601d9a1f598bb5aed8f
jarqprog <jarqprog@gmail.com>
2018-03-04 03:07:24 +0100

    comment anused method (save transaction)

--- src/dao/ShopDAOImpl.java
+++ src/dao/ShopDAOImpl.java
@@ -1,10 +1,6 @@
 package dao;
 
-import managers.ResultSetManager;
-
 import java.sql.Connection;
-import java.util.*;
-
 
 public class ShopDAOImpl implements ShopDAO {
 
@@ -15,12 +11,12 @@ public class ShopDAOImpl implements ShopDAO {
     }
 
     public void saveStudentTransaction(int studentID, int artifactID) {
-        ResultSetManager manager = new ResultSetManager();
-        Date date = new Date();
-        String transactionDate = date.toString();
-        // it will be changed
-        String query = String.format("INSERT INTO students_transactions " +
-                        "VALUES(null, %s, %s, '%s');", studentID, artifactID, transactionDate);
-        manager.inputData(query);
+//
+//        Date date = new Date();
+//        String transactionDate = date.toString();
+//
+//        String query = String.format("INSERT INTO students_transactions " +
+//                        "VALUES(null, %s, %s, '%s');", studentID, artifactID, transactionDate);
+
     }
 }
