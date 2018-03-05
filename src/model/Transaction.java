8987c0581e7b0d416d1b7f9187f4f39340fe9e95
jarqprog <jarqprog@gmail.com>
2018-03-05 19:14:04 +0100

    refactor,

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -8,14 +8,14 @@ public class Transaction {
     private int itemId;
     private int itemPrice;
 
-    public Transaction(int studentId, int itemId, int itemPrice) {
+    Transaction(int studentId, int itemId, int itemPrice) {
         this.studentId = studentId;
         this.itemId = itemId;
         this.itemPrice = itemPrice;
+        this.localDate = LocalDate.now();
     }
 
     public String toString() {
-        String output = String.format("\tDate: %tF, Student: %d, Item: %d, Price: %d", localDate.now(), studentId, itemId, itemPrice);
-        return output;
+        return String.format("\tDate: %tF, Student: %d, Item: %d, Price: %d", localDate, studentId, itemId, itemPrice);;
     }
 }
