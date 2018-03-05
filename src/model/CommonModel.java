a5dffb1db464f50c6a72fb13aadc366ed366e67b
jarqprog <jarqprog@gmail.com>
2018-03-05 18:21:50 +0100

    remove SuppressWarnings

--- src/model/CommonModel.java
+++ src/model/CommonModel.java
@@ -5,7 +5,6 @@ import dao.DaoFactory;
 public abstract class CommonModel {
     // parent class of all BO in app
 
-    @SuppressWarnings("unchecked")
     public void saveModel() {
         String className = getClass().getSimpleName();
         DaoFactory.getByModel(className).saveModel(this);
