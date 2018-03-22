9774b382b8a4eb5e4f5893aac8c55b029d436e24
kamilBoksa <kamil.boksa@gmail.com>
2018-03-22 09:33:51 +0100

    Refactor TransactionTest class to jUnit4

--- src/main/tests/model/TransactionTest.java
+++ src/main/tests/model/TransactionTest.java
@@ -1,15 +1,14 @@
 package model;
 
-import org.junit.jupiter.api.Test;
-
+import org.junit.Test;
 import java.time.LocalDate;
+import static junit.framework.Assert.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class TransactionTest {
+public class TransactionTest {
 
     @Test
-    void constructor_createsProperTransactionObject() {
+    public void constructor_createsProperTransactionObject() {
         Transaction transaction = new Transaction(1, 1, 10);
         LocalDate date  = LocalDate.now();
         String expected = String.format("\tDate: %tF, Student: 1, Item: 1, Price: 10",date);
@@ -17,7 +16,7 @@ class TransactionTest {
     }
 
     @Test
-    void constructor_transactionPriceIsSetToZeroWhenNegativeNumberPassed() {
+    public void constructor_transactionPriceIsSetToZeroWhenNegativeNumberPassed() {
         Transaction transaction = new Transaction(1,1, -100);
         int expected = 0;
         assertEquals(expected, transaction.getItemPrice());
