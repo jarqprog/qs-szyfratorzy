6ac09e3c6de51e67b4abf6a53b6727de61833db9
kamilBoksa <kamil.boksa@gmail.com>
2018-03-22 09:34:16 +0100

    Refactor UserTest class to jUnit4

--- src/main/tests/model/UserTest.java
+++ src/main/tests/model/UserTest.java
@@ -1,28 +1,24 @@
 package model;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
 
- class UserTest {
+public class UserTest {
 
     @Test
-    void getEmail_ShouldReturnProperlyConstructedEmailAdress() {
+    public void getEmail_ShouldReturnProperlyConstructedEmailAdress() {
         User user = new Student("John", "Doe", "123");
         String expected = "john.doe@cc.com";
         assertEquals(expected, user.getEmail());
     }
 
-    @Test
-    void constuctor_numbersInNameShouldThrowIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, ()-> {
-        new Student("Jo4n", "Doe", "123");});
+    @Test(expected = IllegalArgumentException.class)
+    public void constuctor_numbersInNameShouldThrowIllegalArgumentException() {
+        new Student("Jo4n", "Doe", "123");
     }
 
-    @Test
-    void constructor_numbersInSurnameShouldThrowIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, ()-> {
-            new Student("John", "Do5e", "123");});
+    @Test(expected = IllegalArgumentException.class)
+    public void constructor_numbersInSurnameShouldThrowIllegalArgumentException() {
+            new Student("John", "Do5e", "123");
     }
-
 }
